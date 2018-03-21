f41361f4e02614b51c6f081f4901948a998bc64c
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-20 17:48:47 -0700

    we broke the madlib parser

--- madlibs.py
+++ madlibs.py
@@ -1,7 +1,5 @@
 """A madlib game that compliments its users."""
 
-from random import choice
-
 from flask import Flask, render_template, request
 
 # "__name__" is a special Python variable for the name of the current module.
@@ -14,33 +12,61 @@ AWESOMENESS = [
     'smashing', 'lovely',
 ]
 
+ADJECTIVES = ['vivacious', 'explosive', 'slumbery', 'awesome', 'cool']
+
+PETS = [
+    'dog', 'cat', 'balloonicorn', 'magical ferret', 'worm', 'snake', 'yak',
+    'alpaca'
+]
+
 
 @app.route('/')
 def start_here():
     """Display homepage."""
-
-    return "Hi! This is the home page."
-
-
-@app.route('/hello')
-def say_hello():
-    """Say hello to user."""
 
     return render_template("hello.html")
 
 
 @app.route('/greet')
 def greet_person():
-    """Greet user with compliment."""
+    """Greet user."""
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    return render_template("greet.html",
+                           person=player)
 
-    return render_template("compliment.html",
-                           person=player,
-                           compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    player_choice = request.args.get("yes-no")
+    if player_choice == 'no':
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html", adjectives=ADJECTIVES, pets=PETS)
+
+
+@app.route('/madlib')
+def show_madlib():
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    person = request.args.get("person")
+    adjective = request.args.get("adjective")
+
+    pets = []
+    print request.args
+    for arg, value in request.args.items():
+        print arg
+        print value
+        if arg == 'pets':
+            pets.append(value)
+    print pets
+
+    verb = request.args.get("verb")
+
+    return render_template("madlib.html", color=color, noun=noun,
+                           person=person, adjective=adjective, pets=pets,
+                           verb=verb)
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
